d3b2a797a386bef91b59a68caac05a4274831f2e
Chad-Michael Muirhead <chadmuirhead@live.com>
2019-04-22 19:41:23 -0400

    Initial Commit

---
+++ Resuable_segmentControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Resuable_segmentControl
+//
+//  Created by Chad-Michael Muirhead on 4/22/19.
+//  Copyright © 2019 Chad-Michael Muirhead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
